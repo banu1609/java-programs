98e583868d93c3ff8ff1871d6cc3acc8a5000d67
banu1609 <144659335+banu1609@users.noreply.github.com>
2025-01-25 14:00:46 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,9 @@
+class MultiplicationTable
+{
+public static void main(String[] args){
+for(int n=1;n<=10;n++){
+for(int i=1;i<=10;i++){
+System.out.println(n+"X"+i+"="+(n*i));
+}
+System.out.println();
+}}}
